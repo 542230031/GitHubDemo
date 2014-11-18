1e1722eeb9f747f026af4f862756432d346ddadc
542230031 <542230031@qq.com>
2014-11-18 23:28:58 +0800

    no message

---
+++ hhh/hhh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hhh
+//
+//  Created by Tiger on 14-11-18.
+//  Copyright (c) 2014年 Qinghu Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hhh/hhh/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hhh
+//
+//  Created by Tiger on 14-11-18.
+//  Copyright (c) 2014年 Qinghu Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
