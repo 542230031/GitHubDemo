581d9c2266fee279d8df327f5538d3cbd18af5ab
542230031 <542230031@qq.com>
2014-11-21 00:00:43 +0800

    addnew

--- hhh/hhh/ViewController.h
+++ hhh/hhh/ViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+<<<<<<< HEAD
 @property(strong,nonatomic)NSString *name;
 @property(strong,nonatomic)NSString *height;
+=======
+
+>>>>>>> parent of 8054238... name
 @end
